0aec2320d013023134e88c7cb1751fe81470fbfe
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-03-15 19:02:00 +0530

    Add: Card Images

---
+++ config/data.ts
@@ -0,0 +1,30 @@
+export const backgroundImages = [
+  [
+    "/image-1-1.svg",
+    "/image-1-2.svg",
+    "/image-1-3.svg",
+    "/image-1-4.svg"
+  ],
+  [
+    "/image-2-1.svg",
+    "/image-2-2.svg",
+    "/image-2-3.svg"
+  ],
+  [
+    "/project-3-1.svg",
+    "/project-3-2.svg",
+    "/project-3-3.svg",
+    "/project-3-4.svg"
+  ],
+  [
+    "/project-4-1.svg",
+    "/project-4-2.svg",
+    "/project-4-3.svg"
+  ],
+  [
+    "/project-5-1.svg",
+    "/project-5-2.svg",
+    "/project-5-3.svg",
+    "/project-5-4.svg"
+  ]
+];
