900b4eaf492e941512ab1c1950ec274f634f0433
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-03-16 17:58:49 +0530

    Fix: Button variants styling

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,7 +4,7 @@ import { FaGoogle } from "react-icons/fa";
 
 const Auth = () => {
   return (
-    <div className="w-[460px] bg-black/20 p-12 rounded-3xl h-fit backdrop-blur-lg flex flex-col items-center gap-6">
+    <div className="w-[460px] bg-black/20 p-10 rounded-3xl h-fit backdrop-blur-lg flex flex-col items-center gap-6">
       <Image src="/logo.gif" alt="Logo" width={60} height={60} />
       <p className="text-white font-jakarta text-[1.375rem] font-semibold text-center">
         Welcome to Forescribe
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 
 const variants = {
-  primary: "bg-[#8B3DFF] flex  items-center justify-center gap-2  w-full flex justify-center rounded-lg px-3 py-3 hover:opacity-90 text-white",
-  secondary: "bg-[#4A4A4A] flex items-center gap-2 w-full flex justify-center rounded-lg px-3 py-3 hover:opacity-90 text-white",
+  primary: "bg-[#8B3DFF] hover:bg-[#8535FC]",
+  secondary: "bg-[#4A4A4A] hover:bg-[#424242] ",
 };
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary"; 
+  variant?: "primary" | "secondary";
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ variant = "primary", children, ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  children,
+  ...props
+}) => {
   return (
-    <button className={variants[variant]} {...props}>
+    <button
+      className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg px-3 py-3 text-white ${variants[variant]}`}
+      {...props}
+    >
       {children}
     </button>
   );
