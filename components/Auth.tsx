5fad157537251fe2ccb3cd8b2c58dae439020717
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-03-17 08:40:58 +0530

    Add: Entry Animation

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,24 +1,30 @@
+"use client"
 import { FaGoogle, FaMicrosoft } from "react-icons/fa";
 
 import Image from "next/image";
 import Link from "next/link";
 
+import {motion} from "framer-motion";
+
 import Button from "./Button";
 
 const Auth = () => {
   return (
-    <div className="flex w-full flex-col items-center gap-4 px-4 sm:px-6">
-      
-      <div className="flex w-full max-w-[459px] flex-col items-center gap-6 rounded-3xl bg-black/20 p-6 sm:p-8 md:p-10 backdrop-blur-lg">
-        
+    <motion.div 
+    initial={{opacity:0, scale:0}}
+    animate={{opacity:1, scale:1}}
+    transition={{
+                duration: 0.6,
+            }}
+    className="flex w-full flex-col items-center gap-4 px-4 sm:px-6">
+      <div className="flex w-full max-w-[459px] flex-col items-center gap-6 rounded-3xl bg-black/20 p-6 backdrop-blur-lg sm:p-8 md:p-10">
         <Image src="/logo.gif" alt="Logo" width={50} height={50} />
 
-        <p className="font-jakarta text-center text-lg sm:text-xl md:text-2xl font-semibold text-white">
+        <p className="font-jakarta text-center text-lg font-semibold text-white sm:text-xl md:text-2xl">
           Welcome to Forescribe
         </p>
 
-        <div className="font-poppins  flex w-full flex-col gap-4">
-          
+        <div className="font-poppins flex w-full flex-col gap-4">
           <Button variant="primary">
             <FaGoogle className="size-5" />
             Continue with Google
@@ -28,14 +34,12 @@ const Auth = () => {
             <FaMicrosoft className="size-5" />
             Continue with Microsoft
           </Button>
-
         </div>
       </div>
 
-      <p className="font-poppins w-full max-w-[459px] text-left text-xs sm:text-[0.8125rem] text-white/90">
+      <p className="font-poppins w-full max-w-[459px] text-left text-xs text-white/90 sm:text-[0.8125rem]">
         By clicking “Continue with Google/Microsoft” above, you acknowledge that
         you have read and understood, and agree to Forescribe's{" "}
-        
         <Link href="#" className="text-[#8B3DFF] underline">
           Terms & Conditions
         </Link>{" "}
@@ -44,8 +48,7 @@ const Auth = () => {
           Privacy Policy.
         </Link>
       </p>
-
-    </div>
+    </motion.div>
   );
 };
 
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const variants = {
-  primary: "bg-[#8B3DFF] hover:bg-[#8535FC]",
+  primary: "bg-[#8B3DFF] hover:bg-[#7D27F5]",
   secondary: "bg-[#4A4A4A] hover:bg-[#424242] ",
 };
 
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`font-poppins font-extralight flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg p-3 text-white ${variants[variant]}`}
+      className={`font-poppins font-extralight flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg p-3 hover:scale-102 text-white ${variants[variant]}`}
       {...props}
     >
       {children}
